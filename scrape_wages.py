66189ecca3f32d13c01e051fc2e1f31f698e7e7d
yang sun <yangsun@yangs-MacBook-Pro.local>
2022-03-15 13:49:13 -0700

    start compile py file

---
+++ scrape_wages.py
@@ -0,0 +1,96 @@
+
+#####################################################################################
+#
+# The script is scraping GlassDoor pay rate information based on THREE input parameters
+#
+#####################################################################################
+
+# import packages
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+from config import username, password
+import datetime
+import pandas as pd
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from collections import defaultdict
+import sys
+
+
+def init_browser(chromdriver_path):
+    """input chromdriver path for *** MAC users ***
+       chromdriver_path='/Users/yangsun/Downloads/chromedriver 5'
+    """
+    executable_path = {'executable_path': chromdriver_path}
+    browser = Browser('chrome', **executable_path, headless=False)
+    return browser
+
+def sign_in_GlassDoor(username, password):
+    """sign in
+    """
+    # visit url
+    login_url='https://www.glassdoor.com/profile/login_input.htm'
+    browser.visit(login_url)
+    # sign in
+    browser.find_by_id('inlineUserEmail').fill(username)
+    browser.find_by_id('inlineUserPassword').fill(password)
+    browser.find_by_xpath('//button[@type="submit"]').click()
+
+def select_area(area_name):
+    """select area from selector
+    """
+    browser.find_by_xpath('//button[@class="gd-ui-button d-none d-lg-inline-block css-pitbid"]').click()
+    browser.find_by_xpath('//span[@class="SVGInline arrowDown"]')[1].click()
+    browser.find_by_text(area_name).first.click()
+
+def get_frontend_info(pay_list):
+    """Extract front end job title information
+           - front end title
+           - avg total pay
+           - additional pay
+       Return: 
+           {front end title:[avg base pay, base pay, additional pay]}
+    """
+    # front_end_info= {front end title:[avg base pay, base pay, additional pay, Number of reported salaries]}
+    front_end_info=defaultdict(list)
+    title=pay_list.a.strong.text
+    # pay
+    pay=pay_list.span.strong.text
+    # additional pay
+    tag=pay_list.find_all('span',text='Additional Pay')
+    # check if additional pay tag exist
+    if len(tag)==0:
+        additional_pay=0
+    else:
+        additional_pay=tag[0].find_parent('div').strong.text
+    # reports
+    reports=pay_list.find_all('span',class_='css-kkqchx')[-1].text
+    front_end_info[title]=[pay,additional_pay,reports]
+    return front_end_info
+
+def next_page():
+    """click next page button
+    """
+    browser.find_by_xpath('//button[@class="nextButton css-1hq9k8 e13qs2071"]').click()
+
+def get_current_page_info(pay_lists,res:list):
+    """Extract current page pay information
+    """
+    for pay_list in pay_lists:
+        title=pay_list.a.strong.text
+        if title.lower() not in title_list:
+            continue
+        # get front data
+        front_end_info=get_frontend_info(pay_list)
+        res.append(front_end_info)
+    return res
+
+if __name__=='__main__':
+    # define parameters
+    area_name=sys.argv[1]
+    glass_door_scraping_web=sys.argv[2]
+    output_path=sys.argv[3]
+
+    
+
+
